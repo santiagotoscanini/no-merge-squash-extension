1bf7a5cb717e7cad81b5484edfef3e32e82587c4
SofiaTejerina <sofiatejerina23@gmail.com>
2025-09-06 17:52:29 -0300

    change branch

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,25 @@
-import {getActiveTabURL, isGitHubPrTab} from "./utils.js";
+import { getActiveTabURL, isGitHubPrTab } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-        const activeTab = await getActiveTabURL();
+  const activeTab = await getActiveTabURL();
 
-        const container = document.getElementsByClassName("container")[0];
-        if (isGitHubPrTab(activeTab)) {
-            chrome.storage.sync.get(["branchNameRegex"]).then(({branchNameRegex = "main"}) => {
-                container.innerHTML = `
+  const container = document.getElementsByClassName("container")[0];
+  if (isGitHubPrTab(activeTab)) {
+    chrome.storage.sync
+      .get(["branchNameRegex"])
+      .then(({ branchNameRegex = "main" }) => {
+        container.innerHTML = `
                     <div class="title">
                         Squash & merge is going to be blocked if the PR is open.
                         </br>
                         </br>
                         <div class="subtitle">
-                        The current branch name regex is: <code>${branchNameRegex}</code>
+                        The current branch name regex is: <a href="chrome-extension://aabhjeaepficccegjneggegjjcegnhel/options/options.html"><code>${branchNameRegex}</code></a>
                         </div>
                     </div>`;
-            });
-        } else {
-            container.innerHTML = '<div class="title">This is not a GitHub PR page</div>';
-        }
-    }
-);
+      });
+  } else {
+    container.innerHTML =
+      '<div class="title">This is not a GitHub PR page</div>';
+  }
+});
